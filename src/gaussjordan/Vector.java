47be422eb3dbac199c6da0caf4e740dfb54e175a
Marl Christian Ricanor <32050672+Marlcelo@users.noreply.github.com>
2018-07-31 21:26:22 +0800

    Finished RREF
    This update finishes the function rref and can now return the constants vector. Also commented out the print statements.

--- src/gaussjordan/Vector.java
+++ src/gaussjordan/Vector.java
@@ -75,29 +75,15 @@ public class Vector {
             Collections.swap(constants.data, v1, v2);
             Collections.swap(vectors, v1, v2);
             
-//            Vector tempvec = vectors.get(v1);
-//            double[] tempArr  = new double[vectors.get(v1).dimension];
-//            
-//            for(int m = 0; m < vectors.get(v1).dimension; m++){
-//                tempArr[m] = vectors.get(v1).data.get(m);
-//            }
-//            Vector temp = new Vector(tempArr,tempvec.dimension);
-//            vectors.set(v1, vectors.get(v2));
-//            vectors.set(v2, temp);
-//            
-//            //swap the constants as well
-//            double tempCons = constants.data.get(v1);
-//            constants.data.set(v1, constants.data.get(v2));
-//            constants.data.set(v2, tempCons);
         }
         
         public void solve(ArrayList<Vector> vectors, Vector constants, int rowStart){
             double[] tempArr = new double[vectors.get(rowStart).dimension];
             
             for(int n = rowStart+1; n < vectors.size(); n++){ //gets the vector
-                if(vectors.get(n).data.get(rowStart) > 0.00){// will only do row addition if the candidate row is not 0.00
+                if(vectors.get(n).data.get(rowStart) != 0.00){// will only do row addition if the candidate row is not 0.00
                     double scale = vectors.get(n).data.get(rowStart);
-                
+                    //System.out.println("Scale for row addition: "+scale);
                     //scale the constants vector
                     double tempCons = constants.data.get(rowStart) * scale * -1; 
                     //scale the vectors
@@ -108,6 +94,7 @@ public class Vector {
                     Vector temp = new Vector(tempArr,tempArr.length);
                     // performs row addition to the vector 
                     vectors.set(n, temp.add(vectors.get(n)));
+                    // performs row addition to the constant 
                     constants.data.set(n, constants.data.get(n) + tempCons); //adjusts the vectors under the selected vector
                 }
             }
@@ -116,29 +103,37 @@ public class Vector {
         public void ref(ArrayList<Vector> vectors, int dimension, Vector constants){
             int i =0;
             
-            while(i < dimension){
+            while(i < vectors.size()){
                 //scale the row accordingly
                 
-                // find pivot row 
-                int max = i;
-                for (int y = i+1; y < dimension; y++) {
-                    System.out.println("Comparing: "+Math.abs(vectors.get(y).data.get(i))+" AND "+Math.abs(vectors.get(max).data.get(i)));
-                    if (Math.abs(vectors.get(y).data.get(i)) > Math.abs(vectors.get(max).data.get(i))) {
-                        max = y;
+                if(vectors.get(i).data.get(i) == 0){ // if diagonal is 0, swap it!
+                    // find pivot row 
+                    int max = i;
+                
+                    for (int y = i+1; y < dimension; y++) {
+                        //System.out.println("Comparing: "+Math.abs(vectors.get(y).data.get(i))+" AND "+Math.abs(vectors.get(max).data.get(i)));
+                        if (Math.abs(vectors.get(y).data.get(i)) > Math.abs(vectors.get(max).data.get(i))) {
+                            max = y;
+                        }
                     }
+                    //System.out.println("Swapping: "+max+" and "+i);
+                
+                    if(max != i)
+                        swap(vectors,constants,max,i);
                 }
-                System.out.println("Swapping: "+max+" and "+i);
                 
-                if(max != i)
-                swap(vectors,constants,max,i);
-                
+/************************************************************************************************************/   
+
                 double scale = 1.00;
                 
                 for(int x = i; x < dimension; x++){ // for the row being used
-                    if(i == x){ // part of the diagonal
+                    if(i == x && vectors.get(i).data.get(x) != 0.00){ // part of the diagonal
                         scale = vectors.get(i).data.get(x);
                         vectors.get(i).data.set(x, 1.00);
+                        //System.out.println("Before constant scaling: "+constants.data.get(x));
                         constants.data.set(x, constants.data.get(x)/scale);
+                        //System.out.println("After constant scaling: "+constants.data.get(x));
+                        
                         //System.out.println("Scale @ index"+i+": "+scale);
                         //System.out.println("elemt contains at index"+i+": "+vectors.get(i).data.get(x));
                         //System.out.println("Constant @ index"+i+": "+constants.data.get(x));
@@ -152,28 +147,61 @@ public class Vector {
                 //System.out.println(i + "th Iteration for Vector: "+Arrays.toString(vectors.get(i).data.toArray()));
                 //System.out.println(i + "th Iteration for Constants: "+Arrays.toString(constants.data.toArray()));
                 //System.out.println(Arrays.toString(constants.data.toArray()));
-                System.out.println("After "+i+"th iteration, matrix looks like: ");
-                for(int f = 0; f < dimension;f++)
-                    System.out.println(Arrays.toString(vectors.get(f).data.toArray()));
-                
+                //System.out.println("After "+i+"th iteration, matrix looks like: ");
+//                for(int f = 0; f < dimension;f++)
+//                    System.out.println(Arrays.toString(vectors.get(f).data.toArray()));
+//                System.out.println("After "+i+"th iteration, constants looks like: ");
+//                for(int f = 0; f < constants.dimension ;f++)
+//                    System.out.println(constants.data.get(f));
                 i++;
             }
         }
         
         public void rref(ArrayList<Vector> vectors, int dimension, Vector constants){
+            int i = vectors.size()-1; 
+            //System.out.println("*************** AT RREF ********************");
+            while(i >= 0){
+                
+                double[] tempArr = new double[vectors.get(i).dimension];
             
+                if(vectors.get(i).data.get(i) != 0.00){// if the diagonal is not a zero
+                    for(int n = i-1; n >= 0; n--){ //gets the vector
+                        if(vectors.get(n).data.get(i) != 0.00){// will only do row addition if the candidate row is not 0.00
+                            double scale = vectors.get(n).data.get(i);
+                            //System.out.println("Scale for row addition: "+scale);
+                            //scale the constants vector
+                            double tempCons = constants.data.get(i) * scale * -1; 
+                            //scale the vectors
+                            for(int m = 0; m < vectors.get(i).dimension; m++){
+                                tempArr[m] = vectors.get(i).data.get(m) * scale * -1;
+                            }
+                
+                            Vector temp = new Vector(tempArr,tempArr.length);
+                            // performs row addition to the vector 
+                            vectors.set(n, temp.add(vectors.get(n)));
+                            // performs row addition to the constant 
+                            constants.data.set(n, constants.data.get(n) + tempCons); //adjusts the vectors under the selected vector
+                        }
+                    }
+                }else
+                    //System.out.println("Vector "+i+"s diagonal is  0");
+                               
+               
+                i--;
+            }
         }
         
 	/* Gauss-Jordan Elimination function */
 	Vector Gauss_Jordan(ArrayList<Vector> vectors, int dimension, Vector constants) {
-		Vector v = new Vector(dimension);
 		
-                if(vectors.size() != dimension)
-                    return null;
-                //else
-                ref(vectors, dimension,constants);
-                rref(vectors, dimension, constants);
-		return v;			
+		
+            if(vectors.size() != dimension)
+                return null;
+            //else
+            ref(vectors, dimension,constants);
+            rref(vectors, dimension, constants);
+                
+            return constants;			
 	}
 	
 	/* span function */
@@ -200,21 +228,25 @@ public class Vector {
 	public static void main(String[] args) {
 		Integer dimension = 5;
 		double[] arr = {1, 2, 3, 4};
+                double[] arr2 = {2,3,4,5};
+                double[] arr3 = {1,3,5,7};
+                double[] arr4 = {2,7,8,9};
 		Vector v;
 		
 		v = new Vector(dimension);
 		v.show();
-		
+		//double[] arr = {1, 2, 1, 2};
 		v = new Vector(arr, arr.length);
 		v.show();
 		
 		v.scale(1);
 		v.show();
+                
 		
-		double[] arr2 = {2,3,4,5};
-                double[] arr3 = {1,3,5,7};
-                double[] arr4 = {2,7,8,9};
-                double[] arr5 = {2,9,1,4,2};
+		//double[] arr2 = {2,3,3,7};
+                //double[] arr3 = {3,4,5,8};
+                //double[] arr4 = {4,5,7,9};
+                //double[] arr5 = {2,9,1,4,2};
 
                 
                 double[] c = {1,1,1,1};
@@ -232,11 +264,12 @@ public class Vector {
                 list.add(v3);
                 //list.add(v4);
                 
-                v.Gauss_Jordan(list, vc.dimension, vc);
-		System.out.println();
-                for(int i = 0; i < list.size();i++)
-                    System.out.println(Arrays.toString(list.get(i).data.toArray()));
+                vc = v.Gauss_Jordan(list, vc.dimension, vc);
                 
+		System.out.println();
+//                for(int i = 0; i < list.size();i++)
+//                    System.out.println(Arrays.toString(list.get(i).data.toArray()));
+//                System.out.println();
                 System.out.println(Arrays.toString(vc.data.toArray()));
 		
 	}
